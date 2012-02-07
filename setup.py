803142e3358d4de791f30c2239d2a7ae15336be1
Timothée Peignier <timothee.peignier@tryphon.org>
2012-02-07 15:27:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metadata',
-    version='0.1.1',
+    version='0.2',
     description='Metadata for Humans',
     long_description="""Metadata extract metadatas from files.""",
     author='Timothée Peignier',
