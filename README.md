b22513734a8e0ccf5f88e407cfdc46427cfb9983
Timothée Peignier <timothee.peignier@tryphon.org>
2012-02-07 12:49:54 +0100

    first code dump

---
+++ README.md
@@ -0,0 +1,24 @@
+# Metadata for humans
+
+Metadata extract metadata from files, without having to deal with exotic library compilation, complex API or multiple libraries. Give Metadata a file and it will return a dictionnary with your precious metadata.
+
+    >>> from metadata import extract
+    >>> metadata = extract(file)
+    >>> metadata.get('creation_date')
+    datetime.datetime(2010, 9, 28, 7, 51, 8)
+    >>> metadata.get('title')
+    …
+
+## Features
+
+Extract metadata from almost any kind of files : video, music and other.
+
+## Installation
+
+To install metadata, use pip :
+
+	$ pip install metadata
+
+## Acknowledgement
+
+Metadata uses [hachoir](http://hachoir.org) behind the scenes.
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='metadata',
+    version='0.1',
+    description='Metadata for Humans',
+    long_description="""Metadata extract metadatas from files.""",
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/metadata',
+    packages=find_packages(),
+    install_requires=[
+        'hachoir-core==1.3.3',
+        'hachoir-metadata==1.3.3',
+        'hachoir-parser==1.3.4',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
